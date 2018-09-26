f71899876cf663b4dbff8a550d8c198dcc3b8382
Unknown <ismael98r@hotmail.com>
2018-09-26 17:35:24 +0200

    Ejercicio de mostrar cada 2 segundos la hora durante 20 segundos en hilos diferentes añadido

--- src/ejercicio_hora/Hilo_Hora.java
+++ src/ejercicio_hora/Hilo_Hora.java
@@ -1,8 +1,23 @@
 package ejercicio_hora;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Hilo_Hora implements Runnable {
     @Override
     public void run() {
+        Date date;
+        DateFormat simplyFormat = new SimpleDateFormat("HH:mm:ss");
 
+        for (int i=0;i<10;i++){
+            date = new Date();
+            System.out.printf("Hora: %s\n", simplyFormat.format(date));
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/ejercicio_hora/Main_Segundos.java
+++ src/ejercicio_hora/Main_Segundos.java
@@ -3,14 +3,16 @@ package ejercicio_hora;
 public class Main_Segundos {
 
     public static void main(String[] args) {
-        Thread.sleep(20000);
 
+
+        Thread hilo = new Thread(new Hilo_Hora());
+        hilo.start();
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.printf("\nSe terminó");
     }
-
-
-
-
-
-
 
 }
